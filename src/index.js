2cfe705cf930017f8e78b4e1cddeba609dce73b8
Your Name <amos.pun1994@gmail.com>
2023-02-05 18:23:36 +0545

    pic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <HashRouter>
             <App />
-      </HashRouter>
 )
